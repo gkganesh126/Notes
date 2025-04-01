14db6416e217f2ef7c659c20c97bf3728a697d13
gkganesh126 <gkganesh126@gmail.com>
2025-04-01 21:06:55 +0530

    C- go comparison in finding size

--- DS&algo/code/binary-search.c
+++ DS&algo/code/binary-search.c
@@ -15,7 +15,8 @@ int binarySearch(int arr[], int l, int r, int x) {
 
 int main() {
 	int arr[] = {2,4,5,6,7,8,9}, x=1;
-	int n = sizeof(arr)/sizeof(arr[0]);
+	// note that to find length in golang len() is used.
+	int n = sizeof(arr)/sizeof(arr[0]); 
 	int idx = binarySearch(arr, 0, n, x);	
 	if (idx == -1) 
 		printf("Element not found");
