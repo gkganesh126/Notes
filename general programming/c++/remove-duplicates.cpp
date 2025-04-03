c864bae19d764f32cc46ccdd0e999c7cb1d1c7d2
gkganesh126 <gkganesh126@gmail.com>
2025-04-03 21:06:07 +0530

    Work on remove duplicates

--- general programming/c++/remove-duplicates.cpp
+++ general programming/c++/remove-duplicates.cpp
@@ -2,32 +2,25 @@
 
 using namespace std;
 
-// need to improve
-void removeDuplicate(int len, char *str, vector<char> *sol) {
-    for(int i=0; i<len; i++) {
-        if (str[i] != str[i+1]) {
-            //cout<<"str[i] " << i << " " <<str[i];
-            (*sol).push_back(str[i]);
+int main() {
+    // "mississippi"
+    string str1 = "careermonk", str2;
+    int i, len1;
+    
+    len1 = str1.size();
+cout<<"test: "<<str1[0]<<" "<<str1[1]<<" "<<str1[0]+str1[1];
+    for(i=0; i<len1; i++) {
+        
+        if( str1[i] != str1[i+1]) {
+            str2.push_back(str1[i]);
+            //str2.push_back(str1[i+1]);
+            cout<<"i:"<<i<<"\nstr2: "<<str2<<"\n";
             i++;
-        }else {
+        }
+        else {
             i+=2;
         }
-    }
+    }    
+cout<<"\n ans: "<<str2;
+
 }
-
-int main() {
-    //char *str="mississippi";
-    char *str="careermonk";
-    int i=0, len=strlen(str);
-    vector<char> sol;
-
-    removeDuplicate(len, str, &sol);
-    //removeDuplicate(len, str, &sol);
-    
-    
-    //cout<<str<< "\n";
-    for(auto s : sol) {
-        cout<<s<<" ";
-    }
-}
-
