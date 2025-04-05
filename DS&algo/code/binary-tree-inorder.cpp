6bcb468c407e12a64b32261344b98bab33e43b46
gkganesh126 <gkganesh126@gmail.com>
2025-04-05 21:25:23 +0530

    Inorder traversal of binary tree in C

---
+++ DS&algo/code/binary-tree-inorder.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct BinaryTreeNode {
+    int data;
+    struct BinaryTreeNode * left;
+    struct BinaryTreeNode * right;
+};
+
+void inorderTraversal(struct BinaryTreeNode * root) {
+    if (!root)
+        return;
+    inorderTraversal(root->left);
+    cout<<root->data;
+    inorderTraversal(root->right);
+}
+int main() {
+    struct BinaryTreeNode * node = 
+        (struct BinaryTreeNode*) malloc(sizeof(struct BinaryTreeNode));
+    node->left = 
+        (struct BinaryTreeNode*) malloc(sizeof(struct BinaryTreeNode));
+    node->right = 
+        (struct BinaryTreeNode*) malloc(sizeof(struct BinaryTreeNode));
+        
+    node->data = 1;
+    node->left->data=2;
+    node->left->left=NULL;
+    node->left->right=NULL;
+    node->right->data=3;
+    node->right->left=NULL;
+    node->right->right=NULL;
+
+    inorderTraversal(node);
+
+    free(node);
+    free(node->left);
+    free(node->right);
+}
