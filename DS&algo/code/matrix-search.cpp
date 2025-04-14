70106f2a20d4e44ae3df875d3c0dab1c64f824d4
gkganesh126 <gkganesh126@gmail.com>
2025-04-14 15:55:11 +0530

    modify matrix search cpp

--- DS&algo/code/matrix-search.cpp
+++ DS&algo/code/matrix-search.cpp
@@ -7,10 +7,13 @@ int main() {
     vector<vector<int> > mat = { { 1, 5, 9, 11 },
                                  { 14, 20, 21, 26 },
                                  { 30, 34, 43, 50 } };
-    int i, j, elementToSearch=20;
+    int i, j, elementToSearch=50;
 
+cout<<"mat.size(): "<<mat.size()<<"\n";
+cout<<"mat[0].size():"<<mat[0].size()<<"\n";
     for(i=0; i<mat.size(); i++) {
         for(j=0; j<mat[0].size(); j++) {
+            cout<<mat[i][j]<<" ";
             if (mat[i][j] == elementToSearch) {
                 cout<<elementToSearch<<" is present";
                 goto l;
